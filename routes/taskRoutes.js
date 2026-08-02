502d5c2e28c8c5c617cfa0ae850ef820180e17d0
sali-12-m <salimmoh870@gmail.com>
2026-08-02 23:30:49 +0300

    Remove commented descriptions from task routes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,20 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const taskController = require("../controllers/taskController");
-
-// GET all tasks
 router.get("/", taskController.getAllTasks);
 
-// GET task by ID
 router.get("/:id", taskController.getTaskById);
-
-// Create a new task
 router.post("/", taskController.createTask);
-
-// Update a task
 router.patch("/:id", taskController.updateTask);
 
-// Delete a task
 router.delete("/:id", taskController.deleteTask);
 
 module.exports = router;
